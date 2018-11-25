711ab80834aefcdc2c84cd15dfbd36f0503b1de6
Daniel Ugrin <ugrindaniel@gmail.com>
2018-11-26 00:21:38 +0100

    Content added Gallery images updated Intro video changed  ( static image for phones )

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,6 +1,61 @@
 $(document).ready(function () {
+    changeNavbarStyle();
+
     $(document).scroll(function () {
-        var $nav = $(".navbar");
-        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+       changeNavbarStyle();
     });
+    
+    function changeNavbarStyle(){
+        var $nav = $(".navbar");
+        $nav.toggleClass('scrolled', $(document).scrollTop() > $nav.height());
+    }
+
+    $(".nav-item").click(function(){
+        event.preventDefault();
+        
+        $(".nav-item").removeClass("active");
+        $(this).addClass("active");
+        
+        var selector = $(this).find(".nav-link").attr("href");
+        var topPosition = $(selector).offset().top;
+        var offset = $(".navbar").outerHeight();
+        
+        var body = $("html, body");
+        body.stop().animate({ scrollTop: topPosition-offset }, 500, 'swing');
+
+    });
+
+    //gallery
+    var $grid = $('.grid').masonry({
+        // options
+        itemSelector: '.grid-item',
+        columnWidth: '.grid-sizer',
+        percentPosition: true
+    });
+
+    $grid.imagesLoaded().progress(function () {
+        $grid.masonry('layout');
+    });
+  
+
+    $("#printing input").keyup(function(){
+        calculatePrice();
+    });
+
+    $("#calculate").click(function(){
+        calculatePrice();
+    });
+
+    function calculatePrice(){
+        var x = parseInt($("#xsize").val());
+        var y = parseInt($("#ysize").val());
+        var z = parseInt($("#zsize").val());
+        
+        var magicConstant = 0.003925233;
+
+        var cost = x*y*z*magicConstant;
+        console.log(cost);
+        if (isNaN(cost)) $("#price").text("Töltsd ki a mezőket!");
+        else $("#price").text(cost+" Ft");
+    }
 });
